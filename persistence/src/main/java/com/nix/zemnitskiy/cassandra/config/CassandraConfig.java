253c1c77116ed4e8f76509b62e9158b7ca21d8e7
nikita <nik.zemnitskiy@gmail.com>
2020-06-22 18:53:06 +0300

    docker has been added

--- persistence/src/main/java/com/nix/zemnitskiy/cassandra/config/CassandraConfig.java
+++ persistence/src/main/java/com/nix/zemnitskiy/cassandra/config/CassandraConfig.java
@@ -3,13 +3,19 @@ package com.nix.zemnitskiy.cassandra.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
+import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.mapping.BasicCassandraMappingContext;
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
+import org.springframework.data.cassandra.core.mapping.SimpleUserTypeResolver;
 
 import java.util.Collections;
 
 @Configuration
-public class CassandraConfig {
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
     @Value("${spring.data.cassandra.keyspace-name}")
     private String keyspace;
 
@@ -26,6 +32,29 @@ public class CassandraConfig {
                         .ifNotExists()
                         .withSimpleReplication()));
         return cluster;
+    }
+
+    @Override
+    protected String getKeyspaceName() {
+        return keyspace;
+    }
+
+    @Override
+    public String[] getEntityBasePackages() {
+        return new String[]{"com.nix.zemnitskiy.api"};
+    }
+
+    @Bean
+    public CassandraMappingContext mappingContext() {
+        BasicCassandraMappingContext mappingContext = new BasicCassandraMappingContext();
+        mappingContext.setUserTypeResolver(new
+                SimpleUserTypeResolver(cluster().getObject(), keyspace));
+        return mappingContext;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
     }
 
 }
